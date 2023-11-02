78a98db878d3270363d88548cbca7a44e77b606f
Lauren Puumala <lpuumala@student.ubc.ca>
2023-11-02 10:40:20 -0700

    Added dataset to package

---
+++ R/patients.R
@@ -0,0 +1,12 @@
+#' Patients data
+#'
+#' Made up data for illustration purposes
+#'
+#' @format ## `patients`
+#' A data frame with 3 rows and 2 columns:
+#' \describe{
+#'   \item{id}{ID}
+#'   \item{age}{Patient age}
+#'   ...
+#' }
+"patients"
